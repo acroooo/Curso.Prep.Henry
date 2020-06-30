78fd97c2edc68199785ff816ad4ea4ee8b73db52
Hernán Chamorro <lc.hernanchamorro@gmail.com>
2020-06-30 18:09:42 -0300

    homework 3 finished // homework 4 to finish

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -1,3 +1,4 @@
+
 // No cambies los nombres de las funciones.
 
 function crearGato(nombre, edad) {
@@ -6,14 +7,32 @@ function crearGato(nombre, edad) {
   // Agrega un método (funcion) llamado "meow" que devuelva el string "Meow!"
   // Devuelve el objeto
   // Tu código:
-}
+  // lo que hice yo
+  
+  var objetoGato = {
+    nombre:nombre,
+    edad : edad,
+    meow : function() {
+      return 'Meow!';
+    }
+  }
+  return objetoGato; //esto me falto.
 
+  /*
+
+  */
+}
+// agregarPropiedad({}, 'nombre')
 
 function agregarPropiedad(objeto, property) {
   // Agrega una propiedad al objeto (argumento "objeto") con el valor `null`
   // Devuelve el objeto
   // NOTA: El nombre de la propiedad no es "propiedad", el nombre es el valor del argumento llamado "property" (una cadena/string)
   // Tu código:
+  /*
+  */
+  objeto[property] = null;
+  return objeto;
 }
 
 function invocarMetodo(objeto, metodo) {
@@ -21,6 +40,7 @@ function invocarMetodo(objeto, metodo) {
   // Invoca ese método
   // Nada necesita ser devuelto ("returned")
   // Tu código:
+  objeto.metodo;
 }
 
 function multiplicarNumeroDesconocidoPorCinco(objetoMisterioso) {
